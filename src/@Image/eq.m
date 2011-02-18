340779ee1354de03adbf63723c0970b4bb985825
dlegland <david.legland@nantes.inra.fr>
2011-02-18 15:40:26 +0000

    Image/eq: fix bug in function eq

--- src/@Image/eq.m
+++ src/@Image/eq.m
@@ -1,25 +1,25 @@
 function res = eq(this, arg)
-%EQ Overload the eq operator for Imaeq objects
+%EQ Overload the eq operator for Image objects
 %
 %   output = eq(input)
 %
-%   Exampeq
+%   Example
 %   eq
 %
 %   See also
 %
 %
 % ------
-% Author: David eqgland
-% e-mail: david.eqgland@grignon.inra.fr
+% Author: David legland
+% e-mail: david.legland@grignon.inra.fr
 % Created: 2010-11-28,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2010 INRA - Cepia Software Platform.
 
-if isa(arg, 'Imaeq')
+if isa(arg, 'Image')
     arg = arg.data;
 end
 
 newData = bsxfun(@eq, this.data, arg);
 
-nd = eqtDimension(this);
-res = Imaeq(nd, 'data', newData, 'parent', this, 'type', 'binary');
+nd = getDimension(this);
+res = Image(nd, 'data', newData, 'parent', this, 'type', 'binary');
