2e3849be26c2beaf1ff9543ad880ac68e3294fa9
dlegland <david.legland@nantes.inra.fr>
2011-12-15 17:49:41 +0000

    add clone function

---
+++ src/@Image/clone.m
@@ -0,0 +1,19 @@
+function res = clone(this)
+%CLONE  Create a deep-copy of an image object
+%
+%   output = clone(input)
+%
+%   Example
+%   clone
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2011-12-15,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2011 INRA - Cepia Software Platform.
+
+% create new image object for storing result
+res = Image(ndims(this), 'data', this.data, 'parent', this);
