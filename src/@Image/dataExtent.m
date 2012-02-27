407c9b2575238163460cb15919a4c6166140c350
dlegland <david.legland@nantes.inra.fr>
2012-02-27 09:13:43 +0000

    add grayscaleExtent.m and dataExtent.m

---
+++ src/@Image/dataExtent.m
@@ -0,0 +1,40 @@
+function varargout = dataExtent(this, varargin)
+%DATAEXTENT Intensity extent of image data 
+%
+%   EXTENT = dataExtent(IMG)
+%   [VMIN VMAX] = dataExtent(IMG)
+%   Returns minimal and maximal intensity values in image. 
+%
+%   Example
+%     img = Image.read('cameraman.tif');
+%     dataExtent(img)
+%     ans = 
+%         7   253
+%
+%   See also
+%   min, max, grayscaleExtent
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2012-02-27,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2012 INRA - Cepia Software Platform.
+
+if isempty(varargin)
+    % compute minimal and maximal values
+    minVal = double(min(this.data(:)));
+    maxVal = double(max(this.data(:)));
+else
+    % compute minimal and maximal values within a mask
+    roi = varargin{1};
+    minVal = double(min(this.data(roi)));
+    maxVal = double(max(this.data(roi)));
+end
+
+
+% format output
+if nargout <= 1
+    varargout = {[minVal maxVal]};
+else
+    varargout = {minVal, maxVal};
+end
---
+++ src/@Image/grayscaleExtent.m
@@ -0,0 +1,40 @@
+function varargout = grayscaleExtent(this)
+%GRAYSCALEEXTENT Grayscale extent of input image data type
+%
+%   EXTENT = grayscaleExtent(IMG)
+%   [GMIN GMAX] = grayscaleExtent(IMG)
+%   Returns minimal and maximal values of image data type. If image is of
+%   type double, returns the minimal and maximal values of image data.
+%   Useful for computing appropriate display scaling.
+%
+%   Example
+%     img = Image.read('cameraman.tif');
+%     grayscaleExtent(img)
+%     ans = 
+%         0   255
+%
+%   See also
+%   intmin, intmax, min, max, dataExtent
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2012-02-27,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2012 INRA - Cepia Software Platform.
+
+% compute minimal 
+if isinteger(this.data)
+    type = class(this.data);
+    minVal = double(intmin(type));
+    maxVal = double(intmax(type));
+else
+    minVal = double(min(this.data(:)));
+    maxVal = double(max(this.data(:)));
+end
+
+% format output
+if nargout <= 1
+    varargout = {[minVal maxVal]};
+else
+    varargout = {minVal, maxVal};
+end
