43124bf66d2cdca751dee8a0de5361c41a89a047
dlegland <david.legland@nantes.inra.fr>
2016-10-18 10:14:50 +0200

    fix order for 3D color images and update doc

--- src/@Image/write.m
+++ src/@Image/write.m
@@ -4,16 +4,20 @@ function write(this, filename, varargin)
 %   output = write(input)
 %
 %   Example
-%   write
+%     % read a color image and write a new image
+%     img = Image.read('peppers.png');
+%     img2 = flip(img, 1);
+%     write(img2, 'colorImage.tif');
 %
 %   See also
 %   Image/read
 %
+
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@nantes.inra.fr
 % Created: 2011-06-14,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2011 INRA - Cepia Software Platform.
 
 % assumes format can be managed by Matlab Image Processing
-imwrite(permute(this.data, [2 1 3 4 5]), filename, varargin{:});
+imwrite(permute(this.data, [2 1 4 3 5]), filename, varargin{:});
