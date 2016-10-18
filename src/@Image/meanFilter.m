3bfcbb83a614d328d69dd6461fdb4e276b7e54c0
dlegland <david.legland@nantes.inra.fr>
2016-10-18 10:15:27 +0200

    update doc

--- src/@Image/meanFilter.m
+++ src/@Image/meanFilter.m
@@ -20,9 +20,10 @@ function res = meanFilter(this, se, varargin)
 %   See also:
 %   medianFilter, gaussianFilter, filter, imfilter
 %
+
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@inra.fr
 % Created: 2011-08-05,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2011 INRA - Cepia Software Platform.
 
