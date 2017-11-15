5aab53963ba56b5dbdcd83727ef68c95b95eef9a
dlegland <david.legland@inra.fr>
2017-11-15 14:50:29 +0100

    add clearCalibration method

--- src/@Image/physicalSize.m
+++ src/@Image/physicalSize.m
@@ -5,16 +5,17 @@ function s = physicalSize(this)
 %
 %   Example
 %   img = Image2D.read('cameraman.tif');
-%   img.physicalSize()
+%   physicalSize(img)
 %   ans =
 %       256 256
 %
 %   See also
-%   getDimension
+%   ndims, size, physicalExtent, isCalibrated, clearCalibration
 %
+
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@inra.fr
 % Created: 2010-07-13,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2010 INRA - Cepia Software Platform.
 
