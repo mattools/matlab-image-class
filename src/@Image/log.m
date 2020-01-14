d3b03d4061cf9161746a0baf479a927025aeb7f9
dlegland <david.legland@inra.fr>
2020-01-14 17:33:22 +0100

    log.m: update name or resulting image

--- src/@Image/log.m
+++ src/@Image/log.m
@@ -12,10 +12,17 @@ function res = log(obj)
 
 % ------
 % Author: David Legland
-% e-mail: david.legland@inra.fr
+% e-mail: david.legland@inrae.fr
 % Created: 2010-12-02,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2010 INRA - Cepia Software Platform.
 
 newData = log(double(obj.Data));
 
-res = Image('data', newData, 'parent', obj, 'type', 'intensity');
+newName = '';
+if ~isempty(obj.Name)
+    newName = sprintf('log(%s)', obj.Name);
+end
+
+res = Image('data', newData, 'parent', obj, ...
+    'name', newName, ...
+    'type', 'intensity');
